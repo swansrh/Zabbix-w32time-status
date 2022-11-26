52a9a9f3916892dd542c1e348c0aa9a49756c7d0
Rhys Swansbra <r.l.swansbra@gmail.com>
2022-11-26 16:21:01 +1100

    ordered hash table to ensure zabbix get results in order

--- w32tmCommands.ps1
+++ w32tmCommands.ps1
@@ -1,14 +1,10 @@
 $currentSource = w32tm /query /source
 $peerList = w32tm /query /peers
-$w32tmValues = @{Source = $currentSource}
-
-
-ForEach ($Line in $($peerList -split "`r`n")){ #No Blank Lines
-    #Write-Host $Line
-    if ($Line -ne ""){
-        $tempArray = $Line.Split(":")
+$w32tmValues = [ordered]@{"Current Source" = $currentSource}
+ForEach ($Line in $($peerList -split "`r`n")){ #Breaks input into indivual lines
+    if ($Line -ne ""){ #checks if line is epmty
+        $tempArray = $Line.Split(":") #splits based on :
         $w32tmValues.add($tempArray[0], $tempArray[1].Remove(0,1))   #Add to Dictionary
     }
 }
-
 $w32tmValues

a1b38b50d6149b48b3903dd1b34cb0e639ad99d6
Rhys Swansbra <r.l.swansbra@gmail.com>
2022-11-26 16:11:56 +1100

    Splits values and adds them all to a hash table

--- w32tmCommands.ps1
+++ w32tmCommands.ps1
@@ -1,17 +1,14 @@
 $currentSource = w32tm /query /source
-Write-Output $currentSource
-Write-Output ""
-
 $peerList = w32tm /query /peers
-#Write-Output $peerList
+$w32tmValues = @{Source = $currentSource}
+
 
 ForEach ($Line in $($peerList -split "`r`n")){ #No Blank Lines
     #Write-Host $Line
     if ($Line -ne ""){
         $tempArray = $Line.Split(":")
-        #Write-Host $tempArray[1].Remove(0,1)
-        Write-Host $tempArray
-        #Add to Dictionary
+        $w32tmValues.add($tempArray[0], $tempArray[1].Remove(0,1))   #Add to Dictionary
     }
-
 }
+
+$w32tmValues

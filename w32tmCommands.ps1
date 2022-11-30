d0a95249e7932a62f7612f04e73a7d86ac7958a2
Rhys Swansbra <r.l.swansbra@gmail.com>
2022-11-30 19:41:19 +1100

    Working with Json for zabbix

--- w32tmCommands.ps1
+++ w32tmCommands.ps1
@@ -1,10 +1,24 @@
 $currentSource = w32tm /query /source
 $peerList = w32tm /query /peers
-$w32tmValues = [ordered]@{"Current Source" = $currentSource}
+$w32tmValues = "["
+
 ForEach ($Line in $($peerList -split "`r`n")){ #Breaks input into indivual lines
-    if ($Line -ne ""){ #checks if line is epmty
+    if ($Line -eq "" -or $Line -like '*#Peers: *'){ #checks if line is epmty
+     #No lines Added
+    }else{
         $tempArray = $Line.Split(":") #splits based on :
-        $w32tmValues.add($tempArray[0], $tempArray[1].Remove(0,1))   #Add to Dictionary
+        if($tempArray[0] -eq "Peer" -and $tempArray[1].Remove(0,1) -eq $currentSource.Remove($currentSource.Length - 1)){
+            $w32tmValues += '{"Current Source":"True",'
+        }elseif ($tempArray[0] -eq "Peer") {
+            $w32tmValues += '{"Current Source":"False",'
+        }
+        if($tempArray[0] -eq "HostPoll Interval"){
+            $w32tmValues += '"' + $tempArray[0] + '":"' + $tempArray[1].Remove(0,1) + '"},'
+        }else{
+            $w32tmValues += '"' + $tempArray[0] + '":"' + $tempArray[1].Remove(0,1) + '",'
+        }
     }
 }
-$w32tmValues
+$w32tmValues = $w32tmValues.Remove($w32tmValues.Length - 1)
+$w32tmValues += "]"
+Write-Host($w32tmValues)
